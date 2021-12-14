10c68788c1db2c60bf7b3c3f5886f720eeb2cd66
pkth002 <kthach@epri.com>
2021-12-14 18:48:38 -0500

    update to examples

--- pre-processing/resize.py
+++ pre-processing/resize.py
@@ -4,7 +4,7 @@ from PIL import Image
 import os, sys
 import subprocess
 
-path = "../../downloads/test2/"
+path = "../../Documents/TEST/"
 dirs = os.listdir( path )
 
 def resize():
@@ -13,7 +13,7 @@ def resize():
         if extension == '.jpg':
             im = Image.open(path+item)
             f, e = os.path.splitext(path+item)
-            imResize = im.resize((518,388), Image.ANTIALIAS)
+            imResize = im.resize((52,52), Image.ANTIALIAS)
             imResize.save(path + 'Resized/' + item , 'JPEG', quality=90)
 
 resize()
